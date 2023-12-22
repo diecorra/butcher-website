bfebb5e5877b9d11f43d2ba3d45a75c03ca21a0c
diecorra <diego.corradi95@gmail.com>
2023-12-22 22:52:48 +0100

    add animation on home

--- src/app/_components/HomePage.tsx
+++ src/app/_components/HomePage.tsx
@@ -4,11 +4,12 @@ import Title from '../_shared/Title';
 import { TbMeat } from 'react-icons/tb';
 import { openInNewTab } from '../_utils/openURLNewTab';
 import { IoLogoWhatsapp } from 'react-icons/io';
+import 'animate.css/animate.min.css';
 
 const HomePage = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center gap-20 z-10 relative">
-      <div className="flex flex-col items-center gap-4">
+      <div className="flex flex-col items-center gap-4 animate__animated animate__lightSpeedInLeft animate__delay-1s">
         <Title
           text1="CARNI"
           text2="SCELTE"
@@ -18,7 +19,7 @@ const HomePage = () => {
       </div>
       <div className="flex gap-4">
         <div
-          className="p-1 flex items-center gap-1 cursor-pointer bg-gray-800 border-solid border-2 border-white rounded-xl w-fit"
+          className="p-1 flex items-center gap-1 cursor-pointer bg-gray-800 border-solid border-2 border-white rounded-xl w-fit animate__animated animate__fadeInBottomLeft animate__delay-2s"
           onClick={() =>
             openInNewTab(
               `https://api.whatsapp.com/send?phone=${process.env.phoneNumber}`
@@ -28,7 +29,7 @@ const HomePage = () => {
           <IoLogoWhatsapp className="icon block" fill="#25D366" />
           Vuoi ordinare?
         </div>
-        <div className="p-1 flex items-center gap-1 cursor-pointer bg-gray-800 border-solid border-2 border-white rounded-xl w-fit">
+        <div className="p-1 flex items-center gap-1 cursor-pointer bg-gray-800 border-solid border-2 border-white rounded-xl w-fit animate__animated animate__fadeInBottomRight animate__delay-2s">
           <TbMeat className="icon block" fill="red" />
           Menù
         </div>
