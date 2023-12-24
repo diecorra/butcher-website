79b50ff5af2ee3408d1d6e10be6be7686b692a62
diecorra <diego.corradi95@gmail.com>
2023-12-24 20:56:54 +0100

    improvements

--- src/app/_components/HomePage.tsx
+++ src/app/_components/HomePage.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 const HomePage = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center gap-20 z-10 relative">
-      <div className="flex flex-col items-center gap-4 animate__animated animate__lightSpeedInLeft animate__delay-1s">
+      <div className="flex flex-col items-center gap-4 animate__animated animate__lightSpeedInLeft animate__delay-0.7s">
         <Title
           text1="CARNI"
           text2="SCELTE"
@@ -22,13 +22,13 @@ const HomePage = () => {
           target="_blank"
           href={`https://api.whatsapp.com/send?phone=${process.env.phoneNumber}`}
         >
-          <div className="p-1 px-2 flex items-center gap-1 cursor-pointer bg-gray-800 border-solid border-2 border-white rounded-xl w-fit animate__animated animate__fadeInBottomLeft animate__delay-2s hover:border-orange-300">
+          <div className="p-1 px-2 flex items-center gap-1 cursor-pointer bg-gray-800 border-solid border-2 border-white rounded-xl w-fit animate__animated animate__fadeInBottomLeft animate__delay-1s hover:border-orange-300">
             <IoLogoWhatsapp className="icon block" fill="#25D366" />
             Vuoi ordinare?
           </div>
         </Link>
         <Link href="/menu">
-          <div className="p-1 px-3 flex items-center gap-1 cursor-pointer bg-gray-800 border-solid border-2 border-white rounded-xl w-fit animate__animated animate__fadeInBottomRight animate__delay-2s hover:border-orange-300">
+          <div className="p-1 px-3 flex items-center gap-1 cursor-pointer bg-gray-800 border-solid border-2 border-white rounded-xl w-fit animate__animated animate__fadeInBottomRight animate__delay-1s hover:border-orange-300">
             <TbMeat className="icon block" fill="red" />
             Menù
           </div>
