ec1f0aadc7de4ec4ab147c0d15841a1e7add64c0
diecorra <diego.corradi95@gmail.com>
2023-12-25 20:13:19 +0100

    add phone-number in env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    phoneNumber: '393458503417',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
